a87c804ffc55dad83e7d8657ea6ccacbb2a97301
aqdasmalik <aqdasmalikcs@gmail.com>
2018-12-01 23:08:21 +0500

    Theme pages and css added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def category
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+
+  def blog_details
+  end
+
+  def about
+  end
+
+  def error404
+  end
+
+  def login
+  end
+
+  def signup
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/',                  to: 'pages#index',             as: ''
+  get '/contact',           to: 'pages#contact',           as: 'contact'
+  get '/category',          to: 'pages#category',          as: 'category'
+  get '/blog',              to: 'pages#blog',              as: 'blog'
+  get '/blog_details',      to: 'pages#blog_details',      as: 'blog_details'
+  get '/about',             to: 'pages#about',             as: 'about'
+  get '/error404',          to: 'pages#error404',          as: 'error404'
+  get '/login',             to: 'pages#login',             as: 'login'
+  get '/signup',            to: 'pages#signup',            as: 'signup'
 end
